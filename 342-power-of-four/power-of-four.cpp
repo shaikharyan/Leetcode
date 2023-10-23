232ff20f010a9e82b741bea92c007c8ecb3d28cb
Aryan Shaikh <arsahiyansk2000@gmail.com>
2023-10-23 17:48:59 +0530

    Time: 0 ms (100.00%) | Memory: 6.3 MB (47.70%) - LeetSync

---
+++ 342-power-of-four/power-of-four.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    bool isPowerOfFour(int n) {
+           for (int i = 0; i <= 15; i++) {
+            int powerOfFour = pow(4, i);
+            
+            // If we find a power of four equal to 'n', return true
+            if (powerOfFour == n)
+                return true;
+            
+            // If the current power of four is greater than 'n', there's no need to continue
+            if (powerOfFour > n)
+                return false;
+        }
+        
+        // 'n' is not a power of four
+        return false;
+    }
+};
